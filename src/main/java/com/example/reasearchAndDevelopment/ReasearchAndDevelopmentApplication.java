6ce35d42a731c1a4ac70503f5f81d40f16ae8b76
shanuNP <shanunp2016@gmail.com>
2024-08-10 20:04:50 +0530

    feat: base module

---
+++ src/main/java/com/example/reasearchAndDevelopment/ReasearchAndDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.reasearchAndDevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReasearchAndDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReasearchAndDevelopmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reasearchAndDevelopment/ReasearchAndDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reasearchAndDevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReasearchAndDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
